ca1cbcd2c130dba1c155eb719e7d2701037c33f5
Lawrence Behar <lbehar@xtivia.com>
2026-03-19 11:00:45 -0500

    remove console log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import initFramework from '@liferay-editor-custom-fields/framework';
 import { renderClassSelect } from './renderClassSelect/renderClassSelect';
 
 const init: () => void = () => {
-    console.log('init');
     initFramework();
     Liferay.on('EditorCustomFields_FragmenConfig_OnLoad', renderClassSelect);
 }
